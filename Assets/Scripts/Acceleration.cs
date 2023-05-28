8d4c27264852949f028fad8f39582cfbae9f07a9
gawei1206 <92412075+MoraxTan@users.noreply.github.com>
2023-05-28 12:11:20 +0800

    update the connection with arduinoPort testing( not complete)

--- Assets/Scripts/Acceleration.cs
+++ Assets/Scripts/Acceleration.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
 
 public class Acceleration : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class Acceleration : MonoBehaviour
     private Vector3 previousPosition;
     private Vector3 previousVelocity;
     private Vector3 previousAcceleration;
+    private SerialPort arduinoPort; // 串口对象
+    public string portName = "COM3"; // Arduino所连接的串口号
+    public int baudRate = 9600; // 串口波特率
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +20,15 @@ public class Acceleration : MonoBehaviour
         previousPosition = rb.position;
         previousVelocity = Vector3.zero;
         previousAcceleration = Vector3.zero;
+
+        // 初始化串口
+        arduinoPort = new SerialPort(portName, baudRate);
+        arduinoPort.Open();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
         float timeDelta = Time.fixedDeltaTime;
 
         // 计算物体的速度
@@ -41,5 +48,59 @@ public class Acceleration : MonoBehaviour
         // 将平均加速度应用于物体
         rb.AddForce(averageAcceleration, ForceMode.Acceleration);
         Debug.Log(averageAcceleration.normalized);
+
+        // 将加速度转换为信号形式并发送给Arduino
+        string signal = ConvertAccelerationToSignal(averageAcceleration);
+        arduinoPort.Write(signal);
+
+        // 调试信息
+        Debug.Log("Signal: " + signal);
+    }
+
+    // 关闭串口连接
+    private void OnDestroy()
+    {
+        if (arduinoPort != null && arduinoPort.IsOpen)
+        {
+            arduinoPort.Close();
+        }
+    }
+
+    // 将加速度转换为信号形式
+    private string ConvertAccelerationToSignal(Vector3 acceleration)
+    {
+        // 根据加速度值设置对应的信号
+        string signal = "";
+
+        // 根据实际需求设置阈值
+        float threshold = 0.5f;
+
+        if (acceleration.x > threshold)
+        {
+            signal += "1"; // 向右
+        }
+        else if (acceleration.x < -threshold)
+        {
+            signal += "2"; // 向左
+        }
+        else
+        {
+            signal += "0"; // 停止
+        }
+
+        if (acceleration.z > threshold)
+        {
+            signal += "1"; // 向前
+        }
+        else if (acceleration.z < -threshold)
+        {
+            signal += "2"; // 向后
+        }
+        else
+        {
+            signal += "0"; // 停止
+        }
+
+        return signal;
     }
 }
