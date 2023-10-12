3e340b328a711da588b380c2f730ec8d2488fd0b
gawei1206 <92412075+MoraxTan@users.noreply.github.com>
2023-10-12 15:14:52 +0800

    save

---
+++ Assets/Scripts/AcceleFunctions.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AcceleFunctions
+{
+    /* This program used to implement the functions of Acceleration.cs */
+    public static string ConvertYPositionToSignal(Vector3 previousPosition, float currentPositionY, ref string[] signal, ref string state)
+    {
+        if (previousPosition.y > currentPositionY)
+        {
+            signal[2] = "0"; // 向下
+        }
+        else //if(previousAcceleration.y == currentAccelerationY)
+        {
+            signal[2] = "1"; // 向上
+        }
+
+        currentPositionY = previousPosition.y;
+
+        return signal[2];
+    }
+
+    public static string ConvertAccelerationToSignal(Vector3 previousAcceleration, float currentAccelerationX, float currentAccelerationZ, ref string[] signal, ref string state)
+    {
+        if (previousAcceleration.x > currentAccelerationX)
+        {
+            signal[2] = "0"; // 向下
+        }
+        else //if(previousAcceleration.x == currentAccelerationX)
+        {
+            signal[2] = "1"; // 向上
+        }
+
+        currentAccelerationX = previousAcceleration.x;
+        
+        return signal[2];
+    }
+
+    public static bool IsSame(string[] signal)
+    {
+        if (signal[0] == signal[2] && signal[1] == signal[2])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static string CheckEnd(Vector3 position, ref float[] position_x, ref float[] position_y, ref float[] position_z)
+    {
+        // 检查游戏是否结束的代码
+        // 当position.x/y/z连续重复时，结束游戏
+        // 遇到问题：position进行x/y/z比对，同一数据下不会return "stop"
+        position_x[1] = position.x;
+        position_y[1] = position.y;
+        position_z[1] = position.z;
+
+        if (position_x[0] == position_x[1] && position_y[0] == position_y[1] && position_z[0] == position_z[1])
+        {
+            return "stop";
+        }
+        else
+        {
+            position_x[0] = position_x[1];
+            position_y[0] = position_y[1];
+            position_z[0] = position_z[1];
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/Acceleration .cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Acceleration : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 previousPosition;
+    private Vector3 previousVelocity;
+    private Vector3 previousAcceleration;
+    private SerialPort arduinoPort; // 串口对象
+    public string portName = "COM3"; // Arduino所连接的串口号
+    public int baudRate = 9600; // 串口波特率
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        previousPosition = rb.position;
+        previousVelocity = Vector3.zero;
+        previousAcceleration = Vector3.zero;
+
+        // 初始化串口
+        arduinoPort = new SerialPort(portName, baudRate);
+        arduinoPort.Open();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float timeDelta = Time.fixedDeltaTime;
+
+        Vector3 velocity = (rb.position - previousPosition) / timeDelta;
+
+        Vector3 acceleration = (velocity - previousVelocity) / timeDelta;
+
+        previousVelocity = velocity;
+        previousPosition = rb.position;
+        previousAcceleration = acceleration;
+
+        rb.AddForce(previousAcceleration, ForceMode.Acceleration);
+
+        string[] pre = new string[3];
+        pre[2]= ConvertAccelerationToSignal(previousAcceleration);
+        
+        /*if (CheckEnd(rb.position) == "stop")
+        {
+            signal = "stop";
+        }*/
+
+        if (!IsSame(pre))
+        {
+            Debug.Log("Signal: " + pre[2]);
+            arduinoPort.Write(pre[2]);
+        }
+        pre[0] = pre[1];
+        pre[1] = pre[2];
+        
+        // yield return new WaitForSeconds((float)0.5);
+    }
+
+    // 关闭串口连接
+    private void OnDestroy()
+    {
+        if (arduinoPort != null && arduinoPort.IsOpen)
+        {
+            arduinoPort.Close();
+        }
+    }
+
+    string[] signal = new string[3];
+    string state = "none";
+    float currentAccelerationX = 0f;
+    float currentAccelerationZ = 0f;
+
+    float currentPositionY = 0f;
+
+    private string ConvertYPositionToSignal(Vector3 previousPosition)
+    {
+        return AcceleFunctions.ConvertYPositionToSignal(previousPosition, currentPositionY, ref signal, ref state);
+    }
+
+    private string ConvertAccelerationToSignal(Vector3 previousAcceleration)
+    {
+        return AcceleFunctions.ConvertAccelerationToSignal(previousAcceleration, currentAccelerationX, currentAccelerationZ, ref signal, ref state);
+    }
+
+    private bool IsSame(string[] signal)
+    {
+        return AcceleFunctions.IsSame(signal);
+    }
+
+    float[] position_x = new float[2];
+    float[] position_y = new float[2];
+    float[] position_z = new float[2];
+
+    private string CheckEnd(Vector3 position)
+    {
+        return AcceleFunctions.CheckEnd(position, ref position_x, ref position_y, ref position_z);
+    }
+}
